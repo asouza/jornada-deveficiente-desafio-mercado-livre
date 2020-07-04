6d68170551b14a852c7149b0ffd512397e298e6c
alberto souza <alots.ssa@gmail.com>
2020-07-04 20:57:46 -0300

    [seguranca] recebendo usuario logado para adicionar opiniao

--- src/main/java/com/deveficiente/desafiomercadolivre/adicionaopiniao/AdicionaOpiniaoController.java
+++ src/main/java/com/deveficiente/desafiomercadolivre/adicionaopiniao/AdicionaOpiniaoController.java
@@ -5,7 +5,7 @@ import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,29 +13,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.deveficiente.desafiomercadolivre.cadastroprodutos.Produto;
 import com.deveficiente.desafiomercadolivre.cadastrousuario.Usuario;
-import com.deveficiente.desafiomercadolivre.cadastrousuario.UsuarioRepository;
+import com.deveficiente.desafiomercadolivre.compartilhado.UsuarioLogado;
 
 @RestController
 public class AdicionaOpiniaoController {
 
 	@PersistenceContext
 	private EntityManager manager;
-	@Autowired
-	//1
-	private UsuarioRepository usuarioRepository;
 
 	@PostMapping(value = "/api/produtos/{id}/opiniao")
 	@Transactional
-	//1
-	public String adiciona(@RequestBody @Valid NovaOpiniaoRequest request, @PathVariable("id") Long id) {
-		//1
-		Produto produto = manager.find(Produto.class,id);
-		//1
-		Usuario consumidor = usuarioRepository.findByEmail("alberto@deveficiente.com").get();
-		//1
-		Opiniao novaOpiniao =  request.toModel(produto,consumidor);
-		manager.persist(novaOpiniao);		
-		
+	// 1
+	public String adiciona(@RequestBody @Valid NovaOpiniaoRequest request,
+			@PathVariable("id") Long id,@AuthenticationPrincipal UsuarioLogado usuarioLogado ) {
+		// 1
+		Produto produto = manager.find(Produto.class, id);
+		// 1
+		Usuario consumidor = usuarioLogado.get();
+		// 1
+		Opiniao novaOpiniao = request.toModel(produto, consumidor);
+		manager.persist(novaOpiniao);
+
 		return novaOpiniao.toString();
 	}
 
